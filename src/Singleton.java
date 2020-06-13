9a57d896653e67c9f623edab22af3a35bb201802
Mohsin <msis.rubel@gmail.com>
2020-06-13 05:33:02 -0400

    Singleton D.patterns--Scrabble game project need more project

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,13 +1,33 @@
+
+
+
+
+
+
+
+
+/* Scrabble game project>>> Need more work to done
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Singleton {
     // single pattern define a class that has only one instance and provides a global point of access
     // a class must ensure that only single instance should be created and single object can be used by all other classes
     private static Singleton firstInstance = null;
+
+    // hold all my scrabble letters in array
+    String scrabbleLetters = {
+
+    }
     private Singleton(){}
     public static Singleton getInstance(){
         //getInstance globally used Singleton pattern
         if(firstInstance == null){
-            firstInstance = new Singleton();
+            firstInstance = new Singleton();// lazy instantiation
         }
+        return firstInstance;
 
     }
 }
+*/
